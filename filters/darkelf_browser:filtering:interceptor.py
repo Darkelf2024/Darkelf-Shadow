77e6ebb8c08923df490dc16b3666c1fa08a95420
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2026-03-03 00:35:47 -0600

    Remove user-agent spoofing from StealthInterceptor
    Removed user-agent spoofing headers from the interceptor.

--- filters/darkelf_browser:filtering:interceptor.py
+++ filters/darkelf_browser:filtering:interceptor.py
@@ -1,10 +1,5 @@
 from PySide6.QtWebEngineCore import QWebEngineUrlRequestInterceptor, QWebEngineUrlRequestInfo
 from PySide6.QtCore import QUrl
-
-from ..stealth.ua_spoof import (
-    SPOOF_UA, SPOOF_CH_UA, SPOOF_CH_UA_MOBILE, SPOOF_CH_UA_PLATFORM,
-    SPOOF_CH_UA_FULL_VERSION, SPOOF_CH_UA_FULL_VERSION_LIST,
-)
 from ..utils.url_sanitize import sanitize_url_clearurls
 
 class StealthInterceptor(QWebEngineUrlRequestInterceptor):
@@ -13,18 +8,6 @@ class StealthInterceptor(QWebEngineUrlRequestInterceptor):
         self.engine = engine
 
     def interceptRequest(self, info):
-        try:
-            info.setHttpHeader(b"User-Agent", SPOOF_UA.encode("utf-8"))
-            info.setHttpHeader(b"Sec-CH-UA", SPOOF_CH_UA.encode("utf-8"))
-            info.setHttpHeader(b"Sec-CH-UA-Mobile", SPOOF_CH_UA_MOBILE.encode("utf-8"))
-            info.setHttpHeader(b"Sec-CH-UA-Platform", SPOOF_CH_UA_PLATFORM.encode("utf-8"))
-            info.setHttpHeader(b"Sec-CH-UA-Full-Version", SPOOF_CH_UA_FULL_VERSION.encode("utf-8"))
-            info.setHttpHeader(b"Sec-CH-UA-Full-Version-List", SPOOF_CH_UA_FULL_VERSION_LIST.encode("utf-8"))
-            info.setHttpHeader(b"Sec-CH-UA-Arch", b'"x86"')
-            info.setHttpHeader(b"Sec-CH-UA-Bitness", b'"64"')
-            info.setHttpHeader(b"Sec-CH-UA-Model", b'""')
-        except Exception:
-            pass
         qurl = info.requestUrl()
         scheme = (qurl.scheme() or "").lower()
         if "browserleaks.com" in qurl.host():
