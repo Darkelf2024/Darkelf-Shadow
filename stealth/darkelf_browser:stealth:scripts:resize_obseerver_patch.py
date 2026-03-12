8a151397ba0a63a874842ddf39fba6e8f1cbf92d
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2026-03-12 11:16:41 -0500

    Delete stealth directory

--- stealth/darkelf_browser:stealth:scripts:resize_obseerver_patch.py
+++
@@ -1,9 +0,0 @@
-JS = """
-try {
-  new ResizeObserver(() => {}).observe(document.body);
-} catch (e) {}
-window.addEventListener("error", function(e) {
-  if (e && e.message && e.message.indexOf('ResizeObserver loop limit exceeded') > -1)
-    e.preventDefault();
-}, true);
-"""
